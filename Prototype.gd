0a861f9a8d7bae908dabec207a9458ef80174cd5
cactus5050 <fantafan69@gmail.com>
2021-06-12 19:58:18 -0700

    Added Prototype

---
+++ Prototype.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var Counter = 0;
+var weapon_instance = load("res://Weapon.tscn").instance()
+onready var WD = preload("res://WeaponScript.gd").new()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_child(weapon_instance)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+func _input(event): #trigger when any key is pressed
+	if(event is InputEventKey):
+		Counter = (Counter%3)+1;
+		WD.assign(Counter);
+		$Weapon/Icon.texture = load(WD.weaponData["path"])
