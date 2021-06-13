a3ca9cfb1d783e84b68b79b6f134273c1d77c7b9
cactus5050 <fantafan69@gmail.com>
2021-06-12 20:19:58 -0700

    Tested functions in dictionary

--- Prototype.gd
+++ Prototype.gd
@@ -3,6 +3,9 @@ extends Node2D
 var Counter = 0;
 var weapon_instance = load("res://Weapon.tscn").instance()
 onready var WD = preload("res://WeaponScript.gd").new()
+onready var functionDictionary = {
+	1 : testMacro(WD.weaponData["name"])
+}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_child(weapon_instance)
@@ -15,3 +18,7 @@ func _input(event): #trigger when any key is pressed
 		Counter = (Counter%3)+1;
 		WD.assign(Counter);
 		$Weapon/Icon.texture = load(WD.weaponData["path"])
+		functionDictionary[1]
+
+func testMacro(args):
+	print(args)
